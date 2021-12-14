cceae0c067d1c287bc97647ae2b50991701a9a37
iorikyo79 <iorikyo79@gmail.com>
2021-12-15 00:33:36 +0900

    body-parser 적용 - post data를 간단하게 처리 가능. - request.body로 직접 접근 가능
    redirect 변경(기존 writeHead(302) 방식 대신)

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 var fs = require('fs');
+var qs = require('querystring');
+var path = require('path');
 var template = require('./lib/template.js');
 var sanitizeHtml = require('sanitize-html');
-var path = require('path');
-var qs = require('querystring');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended:false}));
 
 // get > route 방식
 app.get('/', function(req,res){ 
@@ -66,19 +69,13 @@ app.get('/create', function(req,res){
 });
 
 app.post('/create_process', function(req,res){
-  var body = '';
-  req.on('data', function(data){
-    body = body + data;
-  });
-  req.on('end', function(){
-    var post = qs.parse(body);
+    var post = req.body;
     var title = post.title;
     var description = post.description;
     fs.writeFile(`data/${title}`, description, 'utf8', function(err){
       res.writeHead(302, {Location: `/?id=${title}`});
       res.end();
-      })
-  });
+    })
 });
 
 app.get('/update/:pageId', function(request, response){
@@ -108,38 +105,24 @@ app.get('/update/:pageId', function(request, response){
 });
 
 app.post('/update_process', function(request, response){
-  var body = '';
-      request.on('data', function(data){
-          body = body + data;
-      });
-      request.on('end', function(){
-          var post = qs.parse(body);
-          var id = post.id;
-          var title = post.title;
-          var description = post.description;
-          fs.rename(`data/${id}`, `data/${title}`, function(error){
-            fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-              response.writeHead(302, {Location: `/?id=${title}`});
-              response.end();
-            })
-          });
-      });
+  var post = request.body;
+  var id = post.id;
+  var title = post.title;
+  var description = post.description;
+  fs.rename(`data/${id}`, `data/${title}`, function (error) {
+    fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
+      response.redirect(`/?id=${title}`);
+    })
+  });
 });
 
 app.post('/delete_process', function(request, response){
-  var body = '';
-  request.on('data', function(data){
-      body = body + data;
-  });
-  request.on('end', function(){
-      var post = qs.parse(body);
-      var id = post.id;
-      var filteredId = path.parse(id).base;
-      fs.unlink(`data/${filteredId}`, function(error){
-        response.writeHead(302, {Location: `/`});
-        response.end();
-      })
-  });
+  var post = request.body;
+  var id = post.id;
+  var filteredId = path.parse(id).base;
+  fs.unlink(`data/${filteredId}`, function (error) {
+    response.redirect('/');
+  })
 });
 
 app.listen(3000, () => console.log('example port 3000'));
